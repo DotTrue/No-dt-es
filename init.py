808568620719b40ebcefb47dbcbdfdb97aedb9ed
DotTrue <partnerka_aviton@mail.ru>
2026-02-12 20:00:34 +0300

    base

--- init.py
+++ init.py
@@ -1,18 +1,45 @@
 from time import localtime
 
+import asyncio
 import aiogram
 from aiogram import Bot, Dispatcher, types
-import time
-import asyncio
-tbot = Bot("8557021561:AAEDonBAtzLjqGEwzEJ7XlTa_HIFqYCNWMc")
-thandl = Dispatcher()
-@thandl.message()
-async def handler(msg: types.InputTextMessageContent):
-    print('hello', msg.from_user.id,f"{localtime().tm_hour}:{localtime().tm_min}:{localtime().tm_sec}")
 
+#bot
+tbot = Bot("8517781384:AAGnekkD_RoQFFdyw5wxPKxPBT61AGsSVhA")
+tl_handle = Dispatcher()
+
+class BotHandler():
+    def __init__(self,handler: Dispatcher,):
+        self.t_handler = handler
+        self.private_session_keys = []
+        self.group_session_keys = []
+        self.debounce = False
+        self.debtime = 0.1
+    async def debouncew(self):
+        self.debounce = True
+        await asyncio.sleep(self.debtime)
+        self.debounce = False
+
+
+    async def init_links(self):
+        @self.t_handler.message()
+        async def handler(msg: types.InputTextMessageContent):
+            if not self.debounce:
+                await self.debouncew()
+
+
+    async def command_list(self):
+        pass
+    async def guidence(self):
+        pass
+    async def get_keys(self):
+        #todo make a DB request to get an generated API keys
+        pass
+
+#updater-entrypoint
 async def main():
     await tbot.delete_webhook(drop_pending_updates=True)
-    await thandl.start_polling(tbot)
+    await tl_handle.start_polling(tbot)
 
 if __name__ == '__main__':
     asyncio.run(main())
